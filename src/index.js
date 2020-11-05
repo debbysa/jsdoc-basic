7be5a71166f741816d070a9d96cfc921c2f05e13
debbysa <deby.silvia23@gmail.com>
2020-11-05 16:06:22 +0700

    feat(variable): add type in variable

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+/**
+ * Student name
+ * @type {string}
+ */
+const studentName = "deby";
+
+/**
+ * Array of grades
+ * @type {Array<number>}
+ */
+const grades = [90, 93, 97, 95];
+
+/**
+ * Todo object
+ * @type {{id: number|string, text: string}}
+ */
+const todo = {
+  id: "23",
+  text: "semangat deb",
+};
